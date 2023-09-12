7e46417ce1cc7b342d08bec2a67a624b4582a22d
Клименко Константин <coder11@web-axioma.ru>
2023-09-12 12:37:22 +0500

    feature: регистрация класса Options

--- local/modules/constklimenko.schemaorg/include.php
+++ local/modules/constklimenko.schemaorg/include.php
@@ -5,3 +5,17 @@
  * Date: 7.09.23
  * Time: 9:20
  */
+if (file_exists(__DIR__ . "/install/module.cfg.php")) {
+    include(__DIR__ . "/install/module.cfg.php");
+};
+
+use Bitrix\Main\Loader;
+Loader::includeModule($arModuleCfg['MODULE_ID']);
+
+$arClasses=array(
+    /* Библиотеки и классы для авто загрузки */
+    'Schemaorg\Options'=>'lib/Options.php',
+
+);
+
+Loader::registerAutoLoadClasses($arModuleCfg['MODULE_ID'], $arClasses);
